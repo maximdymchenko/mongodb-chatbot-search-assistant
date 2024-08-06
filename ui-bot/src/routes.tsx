492fdd025c5859409b7e6d7b21d69bb992ee393b
Maxim Dymchenko <eastsea1020n@gmail.com>
2024-08-05 21:50:52 -0400

    remove no need

--- ui-bot/src/routes.tsx
+++ ui-bot/src/routes.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { Route, Text, Reply } from "@botonic/react";
 import BookHotel from "./actions/BookHotel";
-import BookRestaurant from "./actions/BookRestaurant";
 import Bye from "./actions/Bye";
 import CloseWebview from "./actions/CloseWebview";
 import ExternalSearch from "./actions/ExternalSearch";
 import InfoReservation from "./actions/InfoReservation";
-import MoreHelp from "./actions/MoreHelp";
 import NearMe from "./actions/NearMe";
 import NotFound from "./actions/NotFound";
 import NotSure from "./actions/NotSure";
 import PickLocation from "./actions/PickLocation";
 import SpecificLocation from "./actions/SpecificLocation";
 import Start from "./actions/Start";
-
-const withConfidence = (input, intent) =>
-  input.intents.some((int) => int.label === intent && int.confidence >= 0.4);
 
 export const routes: Route[] = [
   {
@@ -47,18 +42,7 @@ export const routes: Route[] = [
     action: ExternalSearch,
   },
 
-  // {
-  //   path: "greetings",
-  //   input: (i) => withConfidence(i, "Greetings"),
-  //   action: Greetings,
-  // },
-  {
-    path: "not-found",
-    input: (i) => i.intents && i.intents[0].confidence < 0.7,
-    action: NotFound,
-  },
-  { path: "book-restaurant", intent: "BookRestaurant", action: BookRestaurant },
-
+  
   {
     path: "book-hotel",
     payload: /hotel-.*/,
@@ -79,16 +63,18 @@ export const routes: Route[] = [
     payload: /rating-.*/,
     text: /^(bye|good bye|)$/i,
     action: Bye,
-  },
-  // {
-  //   path: "help",
-  //   text: /.*/,
-  //   payload: /help-.*/,
-  //   action: MoreHelp,
-  // },
+  },  
   {
     path: "not-sure",
     text: /.*/,
     action: NotSure,
   },
+
+  // botonic intent way not done
+  {
+    path: "not-found",
+    input: (i) => i.intents && i.intents[0].confidence < 0.7,
+    action: NotFound,
+  },
+  // { path: "book-restaurant", intent: "BookRestaurant", action: BookRestaurant },
 ];
