6a37ca4fcd3848fcd40e715d8f5b4ddc632858d5
Maxim Dymchenko <eastsea1020n@gmail.com>
2024-08-06 20:54:41 -0400

    remove no used plugin

--- ui-bot/src/plugins.ts
+++ ui-bot/src/plugins.ts
@@ -1,11 +1,3 @@
 import { PluginConfig } from "@botonic/core";
 
-export const plugins: PluginConfig<any>[] = [
-  {
-    id: "intent-classification",
-    resolve: require("@botonic/plugin-intent-classification"),
-    options: {
-      locales: ["en"],
-    },
-  },
-];
+export const plugins: PluginConfig<any>[] = [];
